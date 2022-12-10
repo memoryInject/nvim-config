4183c75d450a963758c6ed0cce1b10f973d31ef0
Mahesh MS <msmahesh007@gmail.com>
2022-12-11 04:11:24 +0800

    Fix dapui icons

--- lua/user/dap/dapui.lua
+++ lua/user/dap/dapui.lua
@@ -1,14 +1,32 @@
 local status_dap, dap = pcall(require, "dap")
 if not status_dap then
-	return
+  return
 end
 
 local status_dapui, dapui = pcall(require, "dapui")
 if not status_dapui then
-	return
+  return
 end
 
-dapui.setup()
+dapui.setup({
+  icons = { expanded = "ﱮ", collapsed = "", current_frame = "契" },
+  controls = {
+    -- Requires Neovim nightly (or 0.8 when released)
+    enabled = true,
+    -- Display controls in this element
+    element = "repl",
+    icons = {
+      pause = "",
+      play = "契",
+      step_into = "",
+      step_over = "",
+      step_out = "",
+      step_back = "倫",
+      run_last = "ﰇ",
+      terminate = "",
+    },
+  },
+})
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
